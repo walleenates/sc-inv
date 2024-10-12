94030d071ba2570805005d09fd27a13152631091
Walleen.Ates <walleenates.202100119@gmail.com>
2024-10-12 16:21:52 +0800

    dsda

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4G4hmCoE-y0YdsSBk8Wk9nHEq-MRlwcc",
-  authDomain: "scinventory1-57023.firebaseapp.com",
-  projectId: "scinventory1-57023",
-  storageBucket: "scinventory1-57023.appspot.com",
-  messagingSenderId: "57401651388",
-  appId: "1:57401651388:web:2d5fda989542b9d16da0b9",
-  measurementId: "G-EB1ZL9ZV57"
+  
 };
 
 const app = initializeApp(firebaseConfig);
